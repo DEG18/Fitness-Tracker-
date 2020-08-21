75c7e3064b08aa9fe3baa27da4f103913c2c8197
Luyu Zeng <zengluyu252@gmail.com>
2020-08-21 18:36:48 -0400

    updated server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts",
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   });
 
 // Routes:
